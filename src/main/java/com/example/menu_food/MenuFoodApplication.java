89957e8e5a393266bed68d7182a71e645830f2bc
jvmeira23 <jvmeira98@gmail.com>
2025-01-30 00:12:07 -0300

    initial commit

---
+++ src/main/java/com/example/menu_food/MenuFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.menu_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/menu_food/MenuFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.menu_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
