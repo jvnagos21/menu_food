448268c4a5ce26e624c857f4835627c60b45d07d
jvmeira23 <jvmeira98@gmail.com>
2025-01-31 01:45:08 -0300

    fix/ add @id in food app

--- src/main/java/com/example/menu_food/food/Food.java
+++ src/main/java/com/example/menu_food/food/Food.java
@@ -1,14 +1,14 @@
 package com.example.menu_food.food;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Table(name = "foods")
 @Entity(name = "foods")
 
 public class Food {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    private String image;
     private Integer price;
 }
 
